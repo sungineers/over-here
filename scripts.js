4fa53e886832591e615cdc58c52480a0370a3033
Jacob Parra <jacobisraelparra@gmail.com>
2021-08-18 19:14:44 -0700

    Overlay test

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-document.querySelector('a-scene').addEventListener('loaded', function () {
+//document.querySelector('a-scene').addEventListener('loaded', function () {
     setTimeout(function () {
         document.getElementById('overlay').remove;
     }, 3000);
-});
+//});
