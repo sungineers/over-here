0c164c58b268f5f95b594bb8793113b3390351f0
Jacob Parra <jacobisraelparra@gmail.com>
2021-08-18 20:41:58 -0700

    test

--- scripts.js
+++ scripts.js
@@ -1,5 +1,18 @@
 function generateUrl() {
     document.getElementById('generateBtn').innerHTML = "Regenerate";
 
-    
+
+    navigator.geolocation.getCurrentPosition((position) => {
+
+        let latitude = position.coords.latitude;
+        let longitude = position.coords.longitude;
+
+
+        console.log(latitude);
+
+        
+      });
+
 }
+
+
