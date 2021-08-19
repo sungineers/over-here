9c3d9ddb96ab09498066b79430d9176d0cc3f042
Jacob Parra <jacobisraelparra@gmail.com>
2021-08-18 19:58:52 -0700

    More changes

--- scripts.js
+++ scripts.js
@@ -1,11 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var scene = document.querySelector('a-scene');
-    scene.addEventListener('loaded', function (e) {
-        console.log('hello');
-    });
-});
-
-
-
-
-setTimeout(function(){ document.getElementById('overlay').remove(); }, 3000);
+function generateUrl() {
+    
+}
